cfa6ba2e80910a91ccfc949b3e23a9dbeb954787
Monish <161581790+mozge07@users.noreply.github.com>
2026-04-29 06:55:17 +0530

    checking something

--- day-2/src/config/database.js
+++ day-2/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 function connectToDb(){
-    mongoose.connect("mongodb://monish:gwSDPcwGrAEfz7QQ@ac-qntupdd-shard-00-00.5dgmknu.mongodb.net:27017,ac-qntupdd-shard-00-01.5dgmknu.mongodb.net:27017,ac-qntupdd-shard-00-02.5dgmknu.mongodb.net:27017/?ssl=true&replicaSet=atlas-8yqqt5-shard-0&authSource=admin&appName=Cluster0")
+    mongoose.connect("mongodb://monish:b490zYpDQymuSsBf@ac-qntupdd-shard-00-00.5dgmknu.mongodb.net:27017,ac-qntupdd-shard-00-01.5dgmknu.mongodb.net:27017,ac-qntupdd-shard-00-02.5dgmknu.mongodb.net:27017/?ssl=true&replicaSet=atlas-8yqqt5-shard-0&authSource=admin&appName=Cluster0")
     .then(()=>{
         console.log("connect to database");
     })
