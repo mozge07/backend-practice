57afc3255681a2e1c2cf002b606e962aac1e2fe9
Monish <161581790+mozge07@users.noreply.github.com>
2026-04-29 07:29:35 +0530

    create db with day-1 name, add .env, .gitignore

--- day-2/server.js
+++ day-2/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const app = require("./src/app")
 const connectToDb = require("./src/config/database")
 
@@ -9,3 +10,5 @@ connectToDb()
 app.listen(3000, ()=>{
     console.log("server is running")
 })
+
+b490zYpDQymuSsBf
