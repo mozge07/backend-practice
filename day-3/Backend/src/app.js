c27eb9b36c33482c7807436a303a919b02978807
Monish <161581790+mozge07@users.noreply.github.com>
2026-05-04 23:50:46 +0530

    practice with folder structure

---
+++ day-3/Backend/src/app.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const app = express()
+const noteModel = require("./Models/note.model")
+
+app.use(express.json())
+
+/**
+ * POST /api/notes
+ * to create new note in DB
+ */
+app.post("/api/notes", async (req, res)=>{
+    const {title, description} = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        Message: "note created successfully",
+        note
+    })
+})
+
+/**
+ * GET /api/notes
+ * it fetch all the notes data from mongo DB
+ */
+app.get("/api/notes", async (req, res)=>{
+
+    const note = await noteModel.find()
+
+    res.status(200).json({
+        message: "note fetched successfully",
+        note
+    })
+})
+
+/**
+ * DELETE /api/notes/:id
+ * it delete the note by providing id
+ */
+app.delete("/api/notes/:id", async (req, res)=>{
+     const id = req.params.id
+     await noteModel.findByIdAndDelete(id)
+
+     res.status(200).json({
+        message: "note deleted successfully",
+     })
+})
+
+/**
+ * PATCH /api/notes/:id
+ * it use to update title or description or both by giving id
+ */
+app.patch("/api/notes/:id", async (req, res)=>{
+    const id = req.params.id
+    const {description} = req.body
+
+    await noteModel.findByIdAndUpdate(id, {description})
+
+    res.status(200).json({
+        message: "note updated successfully"
+    })
+})
+
+module.exports = app
