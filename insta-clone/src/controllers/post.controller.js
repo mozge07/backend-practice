c6f55f0e6fc27b96dbb4d60430bb8f7e2c9587ec
Monish <161581790+mozge07@users.noreply.github.com>
2026-05-22 07:47:01 +0530

    feat: implement JWT authentication in post creation
    - added JWT verification to ensure authorized access for creating posts
    - included error handling for missing or invalid tokens
    - updated post creation response to include success message and post details
    - organized image upload to a specific folder in ImageKit

--- insta-clone/src/controllers/post.controller.js
+++ insta-clone/src/controllers/post.controller.js
@@ -1,6 +1,9 @@
 const postModel = require("../models/post.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
+const { post } = require("../routes/post.routes");
+const { Folders } = require("@imagekit/nodejs/resources.js");
 
 const imageKit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -9,12 +12,42 @@ const imageKit = new ImageKit({
 async function createPostController(req, res) {
   console.log(req.body, req.file);
 
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "token is not provided, Unauthorized access",
+    });
+  }
+
+  let decoded = null;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized access",
+    });
+  }
+
+  console.log(decoded);
+
   const file = await imageKit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "test",
+    folder: "insta-clone-imags",
   });
 
-  res.send(file);
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imgUrl: file.url,
+    user: decoded.id,
+  });
+
+  res.status(201).json({
+    message: "post created successfully",
+    post,
+  });
 }
 
 module.exports = { createPostController };
