1e67ef0b696c005a47e6b91cb83825ec11624db7
Monish <161581790+mozge07@users.noreply.github.com>
2026-05-26 22:54:10 +0530

    feat: add unfollow user functionality to user controller and routes
    - implemented unfollowUserController to allow users to unfollow others
    - updated user.routes.js to include a new route for unfollowing users
    - improved API documentation for the new unfollow feature

--- insta-clone/src/controllers/user.controller.js
+++ insta-clone/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const followModel = require("../models/follow.model");
 const userModel = require("../models/user.model");
 
+/** to follow user */
 async function followUserController(req, res) {
   const followerUsername = req.user.username;
   const followeeUsername = req.params.username;
@@ -46,6 +47,31 @@ async function followUserController(req, res) {
   });
 }
 
+/** to unfollow user */
+
+async function unfollowUserController(req, res) {
+  const followerUsername = req.user.username;
+  const followeeUsername = req.params.username;
+
+  const isUserfollowing = await followModel.findOne({
+    follower: followerUsername,
+    followee: followeeUsername,
+  });
+
+  if (!isUserfollowing) {
+    return res.status(200).json({
+      message: `you are not following ${followeeUsername}`,
+    });
+  }
+
+  await followModel.findByIdAndDelete(isUserfollowing._id);
+
+  res.status(201).json({
+    message: `you have unfollowed ${followeeUsername}`,
+  });
+}
+
 module.exports = {
   followUserController,
+  unfollowUserController,
 };
--- insta-clone/src/routes/user.routes.js
+++ insta-clone/src/routes/user.routes.js
@@ -5,7 +5,7 @@ const identifyUser = require("../middleware/auth.middleware");
 const userRouter = express.Router();
 
 /**
- * @route POST /api/users/follow/:userid
+ * @route POST /api/users/follow/:username
  * @description use to follow user
  * @access Private
  */
@@ -13,6 +13,17 @@ userRouter.post(
   "/follow/:username",
   identifyUser,
   userController.followUserController,
+);
+
+/**
+ * @route POST /api/users/unfollow/:username
+ * @description use to unfollow user
+ * @access Private
+ */
+userRouter.post(
+  "/unfollow/:username",
+  identifyUser,
+  userController.unfollowUserController,
 );
 
 module.exports = userRouter;
