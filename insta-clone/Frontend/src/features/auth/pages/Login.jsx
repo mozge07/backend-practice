4ca4e84715c95adabefd89d2b2abcfc6f92d4283
Monish <161581790+mozge07@users.noreply.github.com>
2026-06-01 22:49:46 +0530

    feat: integrate CORS and Axios for improved API communication
    - added CORS middleware to the backend for handling cross-origin requests
    - updated Login and Register components to utilize Axios for API calls
    - enhanced form handling with state management for user input
    - included Axios dependency in package.json and package-lock.json for HTTP requests

--- insta-clone/Frontend/src/features/auth/pages/Login.jsx
+++ insta-clone/Frontend/src/features/auth/pages/Login.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../style/form.scss"
 import { Link } from 'react-router'
+import axios from 'axios'
 
 const Login = () => {
+
+  const [username, setusername] = useState("")
+  const [password, setpassword] = useState("")
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post("http://localhost:3000/api/auth/login",{
+      username,
+      password
+    },{
+      withCredentials: true
+    })
+    .then(res=>{
+      console.log(res.data);
+    })
+  }
+
+
   return (
     <main>
         <div className="form-container">
             <h1>Login</h1>
-            <form>
-                <input type="text" name='username' placeholder='Enter your name' />
-                <input type="text" name='password' placeholder='Enter your password' />
+            <form onSubmit={handleSubmit}>
+                <input onInput={(e)=>{setusername(e.target.value)}} 
+                type="text" 
+                name='username' 
+                placeholder='Enter your name' />
+
+                <input onInput={(e)=>{setpassword(e.target.value)}} 
+                type="password" 
+                name='password' 
+                placeholder='Enter your password' />
                 <button type='submit'>Login</button>
             </form>
 
--- insta-clone/Frontend/src/features/auth/pages/Register.jsx
+++ insta-clone/Frontend/src/features/auth/pages/Register.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router'
+import axios from 'axios'
 
 const Register = () => {
+
+  const [username, setusername] = useState("")
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    axios.post("http://localhost:3000/api/auth/register", {
+      username,
+      email,
+      password
+    },{
+      withCredentials: true
+    })
+    .then(res =>{
+      console.log(res.data);
+    })
+  }
+
+
+
   return (
     <main>
         <div className="form-container">
             <h1>Register</h1>
-            <form>
-                <input type="text" name='username' placeholder='Enter your name' />
-                <input type="text" name='email' placeholder='Enter your email' />
-                <input type="text" name='password' placeholder='Enter your password' />
+            <form onSubmit={handleSubmit}>
+                <input 
+                onInput={(e)=>{setusername(e.target.value)}} 
+                type="text" 
+                name='username' 
+                placeholder='Enter your name' />
+
+                <input onInput={(e)=>{setemail(e.target.value)}} 
+                type="text" 
+                name='email' 
+                placeholder='Enter your email' />
+
+                <input onInput={(e)=>{setpassword(e.target.value)}} 
+                type="password" 
+                name='password' 
+                placeholder='Enter your password' />
+
                 <button type='submit'>Login</button>
             </form>
 
