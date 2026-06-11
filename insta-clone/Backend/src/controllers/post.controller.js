b176270eb7c8579aa0f0a941428e67432fcbbfbe
Monish <161581790+mozge07@users.noreply.github.com>
2026-06-11 07:28:09 +0530

    feat: enhance post feed with like functionality
    - updated getFeedController to include a boolean indicating if a post is liked by the user
    - modified Post component to visually indicate liked posts with a red color
    - added styling for liked posts in feed.scss

--- insta-clone/Backend/src/controllers/post.controller.js
+++ insta-clone/Backend/src/controllers/post.controller.js
@@ -108,7 +108,18 @@ async function likePostController(req, res) {
  * @description to get all post in DB
  */
 async function getFeedController(req, res) {
-  const posts = await postModel.find().populate("user");
+  const posts = await Promise.all(
+    (await postModel.find().populate("user").lean()).map(async (post) => {
+      const isLiked = await likeModel.findOne({
+        user: req.user.username,
+        post: post._id,
+      });
+
+      post.isLiked = Boolean(isLiked);
+
+      return post;
+    }),
+  );
 
   res.status(200).json({
     message: "post fetched successfully",
