a2903d8035658adee8c8f53e2af47b1af78253ee
Monish <161581790+mozge07@users.noreply.github.com>
2026-06-27 23:40:40 +0530

    fix: update CORS configuration to allow multiple origins
    - modified app.js to support CORS for both localhost and production URL

--- insta-clone/Backend/src/app.js
+++ insta-clone/Backend/src/app.js
@@ -9,7 +9,10 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://insta-clone-five-lovat.vercel.app/",
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
   }),
 );
