beb87849c7a539af0db378729def97ef03580240
Monish <161581790+mozge07@users.noreply.github.com>
2026-06-27 22:54:25 +0530

    fix: update server port configuration and add start script
    - modified server.js to use environment variable for port configuration
    - added start script to package.json for running the application

--- insta-clone/Backend/server.js
+++ insta-clone/Backend/server.js
@@ -4,8 +4,8 @@ const connectDB = require("./src/config/database");
 
 connectDB();
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, ()=>{
-    console.log("Server is running on port number 3000")
-})
+app.listen(port, () => {
+  console.log(`Server is running on port number ${port}`);
+});
